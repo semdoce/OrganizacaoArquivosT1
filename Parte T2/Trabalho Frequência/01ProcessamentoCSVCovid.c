2776bbc4f03e9645af0667e23c939c3c2b7089be
semdoce <g.andrade2431@outlook.com>
2026-06-12 20:54:09 -0300

    organizando codigo e tirando variavel nao utilizada

--- Parte T2/Trabalho Frequência/01ProcessamentoCSVCovid.c
+++ Parte T2/Trabalho Frequência/01ProcessamentoCSVCovid.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "CSVParser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // emcc Teste2.c CSVParser.c -lnoderawfs.js -lnodefs.js
-struct Dados{
+struct Dados {
   int numCasos;
   int numMortes;
 };
@@ -12,37 +12,37 @@ typedef struct Dados Tdados;
 
 #define READ_BUF_SIZE 8192
 
-void callback(char** cols, int ncols, void* userData) {
-    Tdados* dados = (Tdados*) userData;
-        if(strcmp(cols[1],"South America") == 0){
-          dados->numCasos += atoi(cols[5]);
-          dados->numMortes += atoi(cols[8]);
-        }
+void callback(char **cols, int ncols, void *userData) {
+  Tdados *dados = (Tdados *)userData;
+  if (strcmp(cols[1], "South America") == 0) {
+    dados->numCasos += atoi(cols[5]);
+    dados->numMortes += atoi(cols[8]);
+  }
 }
 
-int main() {    
+int main() {
   Tdados dados;
-  dados.numCasos=0;
-  dados.numMortes=0;
-  int qt=0;
-  unsigned int total = 0;
-  char* buf = (char*) malloc(READ_BUF_SIZE);
+  dados.numCasos = 0;
+  dados.numMortes = 0;
+  char *buf = malloc(READ_BUF_SIZE);
+
   CSVParser csv;
   CSVParser_init(&csv);
-  FILE *f = fopen("./owid-covid-data.csv","rb");
-  if(f) {
-    qt = fread(buf,1,READ_BUF_SIZE,f);
-    while(qt > 0) {
-      total += qt;
-      CSVParser_processLines(&csv,buf,qt,callback,&dados);
-      qt = fread(buf,1,READ_BUF_SIZE,f);
+  int qt = 0;
+  FILE *f = fopen("./owid-covid-data.csv", "rb");
+  if (f) {
+    qt = fread(buf, 1, READ_BUF_SIZE, f);
+    while (qt > 0) {
+      CSVParser_processLines(&csv, buf, qt, callback, &dados);
+      qt = fread(buf, 1, READ_BUF_SIZE, f);
     }
+    CSVParser_processLines(&csv, "\n", 1, callback, &dados);
     fclose(f);
-    CSVParser_processLines(&csv,"\n",1,callback,&dados);
   }
-   printf("Numero total de casos: %d\n", dados.numCasos);
-   printf("Numero total de mortes: %d\n", dados.numMortes);
-   printf("A cada %.2f casos, um leva a morte\n", (float)dados.numCasos/dados.numMortes);
+  printf("Numero total de casos: %d\n", dados.numCasos);
+  printf("Numero total de mortes: %d\n", dados.numMortes);
+  printf("A cada %.2f casos, um leva a morte\n",
+         (float)dados.numCasos / dados.numMortes);
   free(buf);
   return 0;
 }

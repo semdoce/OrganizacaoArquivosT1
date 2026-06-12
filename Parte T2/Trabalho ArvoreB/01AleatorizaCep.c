fc1c78e79f0bfc69b986cb5014a72c1ddff627c7
semdoce <g.andrade2431@outlook.com>
2026-06-12 20:44:29 -0300

    refactor: automate generation of both cep-1.dat and cep-2.dat files

--- Parte T2/Trabalho ArvoreB/01AleatorizaCep.c
+++ Parte T2/Trabalho ArvoreB/01AleatorizaCep.c
@@ -14,12 +14,7 @@ struct _Endereco {
   char lixo[2];
 };
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    printf("Uso: ./01AleatorizaCep <nomearquivo> //preferencialmente "
-           "\"cep-1.dat\" ou \"cep-2.dat\"\n");
-    return 1;
-  }
+int main() {
   Endereco e;
   int qt;
   FILE *f = fopen("cep.dat", "rb");
@@ -28,21 +23,29 @@ int main(int argc, char **argv) {
     return 1;
   }
   qt = fread(&e, sizeof(Endereco), 1, f);
-  srand(time(NULL));
 
-  FILE *s = fopen(argv[1], "wb");
-  if (!s) {
-    fprintf(stderr, "Erro ao criar o arquivo.\n");
-    return 1;
-  }
+  FILE *cep1 = fopen("cep-1.dat", "wb");
+  FILE *cep2 = fopen("cep-2.dat", "wb");
+  srand(time(NULL));
   while (qt > 0) {
     float random_num = 1.0 * rand() / RAND_MAX;
     if (random_num < 0.8) {
-      fwrite(&e, sizeof(Endereco), 1, s);
+      fwrite(&e, sizeof(Endereco), 1, cep1);
+    }
+    qt = fread(&e, sizeof(Endereco), 1, f);
+  }
+  srand(time(NULL) + 1);
+  fseek(f, 0, SEEK_SET);
+  qt = fread(&e, sizeof(Endereco), 1, f);
+  while (qt > 0) {
+    float random_num = 1.0 * rand() / RAND_MAX;
+    if (random_num < 0.8) {
+      fwrite(&e, sizeof(Endereco), 1, cep2);
     }
     qt = fread(&e, sizeof(Endereco), 1, f);
   }
   fclose(f);
-  fclose(s);
+  fclose(cep1);
+  fclose(cep2);
   return 0;
 }
